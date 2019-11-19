69dc52b645c02c9d214bb20045b446ecb5c37353
Samuel Chan <cchin.samuel@gmail.com>
2019-11-19 18:43:28 +0800

    feat: support telegram shortcuts

---
+++ web_telegram_shortcuts.js
@@ -0,0 +1,48 @@
+function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+function isKeyCode(keyCode, char) {
+    const keyChar = String.fromCharCode(keyCode)
+    return keyChar == char || keyChar == char.toUpperCase();
+}
+
+const intervalId = setInterval(() => {
+    bindShortcuts();
+}, 5000);
+
+function bindShortcuts() {
+    window.removeEventListener('keyup', handleKeyUp);
+    window.addEventListener('keyup', handleKeyUp);
+
+    if (isTelegramPageReady()) {
+        clearInterval(intervalId);
+    }
+
+    window.removeEventListener('click', handleReply)
+    window.addEventListener('click', handleReply)
+}
+
+async function handleReply(e) {
+    if (e.target.closest('.im_history_message_wrap')) {
+        await sleep(50);
+        $('.btn-primary.im_edit_reply_btn')[0].click()
+    }
+}
+
+function isTelegramPageReady() {
+    const inputSearch = document.querySelector('.tg_head_btn.dropdown-toggle');
+    if (inputSearch) return true;
+    else return false;
+}
+
+function handleKeyUp(e) {
+    console.log('Keyboard input new');
+    if (e.shiftKey) {
+        if (isKeyCode(e.keyCode, 'd')) {
+            e.preventDefault();
+            console.log('press shift + d')
+            $('.media_modal_bottom_panel .media_modal_action_btn')[0].click()
+        }
+    }
+}
