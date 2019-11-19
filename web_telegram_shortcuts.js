c35e336be4a1f16066a7dd63b927cc4ca79f8e1b
Samuel Chan <cchin.samuel@gmail.com>
2019-11-19 18:48:15 +0800

    refactor: remove all colons

--- web_telegram_shortcuts.js
+++ web_telegram_shortcuts.js
@@ -1,22 +1,22 @@
 function sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
+    return new Promise((resolve) => setTimeout(resolve, time))
 }
 
 function isKeyCode(keyCode, char) {
     const keyChar = String.fromCharCode(keyCode)
-    return keyChar == char || keyChar == char.toUpperCase();
+    return keyChar == char || keyChar == char.toUpperCase()
 }
 
 const intervalId = setInterval(() => {
-    bindShortcuts();
-}, 5000);
+    bindShortcuts()
+}, 5000)
 
 function bindShortcuts() {
-    window.removeEventListener('keyup', handleKeyUp);
-    window.addEventListener('keyup', handleKeyUp);
+    window.removeEventListener('keyup', handleKeyUp)
+    window.addEventListener('keyup', handleKeyUp)
 
     if (isTelegramPageReady()) {
-        clearInterval(intervalId);
+        clearInterval(intervalId)
     }
 
     window.removeEventListener('click', handleReply)
@@ -25,22 +25,22 @@ function bindShortcuts() {
 
 async function handleReply(e) {
     if (e.target.closest('.im_history_message_wrap') && e.altKey) {
-        await sleep(50);
+        await sleep(50)
         $('.btn-primary.im_edit_reply_btn')[0].click()
     }
 }
 
 function isTelegramPageReady() {
-    const inputSearch = document.querySelector('.tg_head_btn.dropdown-toggle');
-    if (inputSearch) return true;
-    else return false;
+    const inputSearch = document.querySelector('.tg_head_btn.dropdown-toggle')
+    if (inputSearch) return true
+    else return false
 }
 
 function handleKeyUp(e) {
-    console.log('Keyboard input new');
+    console.log('Keyboard input new')
     if (e.shiftKey) {
         if (isKeyCode(e.keyCode, 'd')) {
-            e.preventDefault();
+            e.preventDefault()
             console.log('press shift + d')
             $('.media_modal_bottom_panel .media_modal_action_btn')[0].click()
         }
